5c03e2414a775b8bcb16408e757feca741744a62
Scott Andrews <scott@andrews.me>
2025-12-23 17:51:20 -0500

    Bump component dependencies
    - wasmtime -> 40
    - wasi:* -> 0.2.6
    - wasi:config -> 0.2.0-rc.1
    - componentized:services -> 0.0.4
    
    While upstream wasi interface changes generally have the same shape,
    bumping the version can result in incompatibility is the previous
    version was hard coded.
    
    Signed-off-by: Scott Andrews <scott@andrews.me>

--- hack/wasmtime/build.sh
+++ hack/wasmtime/build.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # TODO sign locally built packages
-apko publish "${SCRIPT_DIR}/apko.yaml" ghcr.io/reconcilerio/wa8s/wasmtime:latest --ignore-signatures --vcs false
+apko publish "${SCRIPT_DIR}/apko.yaml" ghcr.io/reconcilerio/wa8s/wasmtime:latest --ignore-signatures --vcs=false
 
 # crane tag ghcr.io/reconcilerio/wa8s/wasmtime:latest <version>
